47feaaa0cd4441e498502ae096e1500aa4371748
basra66 <101937603+basra66@users.noreply.github.com>
2022-06-07 18:57:12 +0500

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form style={{margin:'20px'}}>
+        <div className='autocomplete' style={{width:'300px'}}>
+          <input id="myInput" type="text" name="myCountry" placeholder="Country"/>
+        </div>
+        <input type="submit"/>
+      </form>
     </div>
   );
 }
